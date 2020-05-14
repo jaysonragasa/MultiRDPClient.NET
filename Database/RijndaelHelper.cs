4c07972834d744e89fe0ab448318058b9ea72598
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-05-15 02:48:22 +0800

    Rijndael cleanup

--- Database/RijndaelHelper.cs
+++ Database/RijndaelHelper.cs
@@ -1,21 +1,21 @@
 ﻿using RijndaelEncryptDecrypt;
 
 public class RijndaelSettings
+{
+    static string PassPhrase = "4e7046087d6e8eefe1b41bddf8bde56c";
+    static string SaltValue = "36ec281f88fb483b5726ccfc81bae6d9";
+    static string HashAlgorithm = "SHA1";
+    static int PasswordIterations = 2;
+    static string InitVector = "@1B2c3D4e5F6g7H8";
+    static int KeySize = 256;
+
+    public static string Encrypt(string PlainText)
     {
-        public static string PassPhrase = "4e7046087d6e8eefe1b41bddf8bde56c";
-        public static string SaltValue = "36ec281f88fb483b5726ccfc81bae6d9";
-        public static string HashAlgorithm = "SHA1";
-        public static int PasswordIterations = 2;
-        public static string InitVector = "@1B2c3D4e5F6g7H8";
-        public static int KeySize = 256;
-
-        public static string Encrypt(string PlainText)
-        {
-            return Rijndael.Encrypt(PlainText, PassPhrase, SaltValue, HashAlgorithm, PasswordIterations, InitVector, KeySize);
-        }
-
-        public static string Decrypt(string cipherText)
-        {
-            return Rijndael.Decrypt(cipherText, PassPhrase, SaltValue, HashAlgorithm, PasswordIterations, InitVector, KeySize);
-        }
+        return Rijndael.Encrypt(PlainText, PassPhrase, SaltValue, HashAlgorithm, PasswordIterations, InitVector, KeySize);
     }
+
+    public static string Decrypt(string cipherText)
+    {
+        return Rijndael.Decrypt(cipherText, PassPhrase, SaltValue, HashAlgorithm, PasswordIterations, InitVector, KeySize);
+    }
+}
